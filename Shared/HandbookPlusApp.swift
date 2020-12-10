7d8076f730b1305763c1517c29ff1e4b924971a6
Sergei <volkovsa@vsa.su>
2020-12-10 15:48:50 +0500

    Initial Commit

---
+++ Shared/HandbookPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandbookPlusApp.swift
+//  Shared
+//
+//  Created by Sergey Volkov on 10.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct HandbookPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
